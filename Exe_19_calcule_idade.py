ac4893999fc65ae2600b7988244ae8068b85ea7a
bruno_johs@hotmail.com <bruno_johs@hotmail.com>
2024-06-19 22:28:41 -0300

    Novo Exercício 19 calculo de idade

---
+++ Exe_19_calcule_idade.py
@@ -0,0 +1,27 @@
+# Autor:Bruno Manoel
+# Data: 19/06/2024
+# Versão: 1.0
+# Algorítimo: Idade
+# Descrição: Faça um programa que retorne a idade 
+# ====================================================
+
+idade = 0
+resultado_calculo = 0
+# hoje = 19/06/2024
+
+idade = int(input("digite o ano de nascimento:"))
+resultado_calculo = (2024 - idade)
+print(resultado_calculo)
+
+
+#import datetime    
+#tempo = datetime.date.today()    
+#dia = int(input('Dia de Nascimento: '))    
+#mes = int(input('Mês de Nascimento: '))    
+#ano = int(input('Ano de nascimento: '))    
+#tempo2 = datetime.date(day=dia, month=mes, year=ano)    
+#alistamento = tempo - tempo2    
+#print(alistamento)
+
+    
+
